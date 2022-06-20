19e989a630ed3b3c488e96dc3c59294f01105f51
0immi <nyi0944@unist.ac.kr>
2022-06-21 02:04:06 +0900

    add velocity pkg

---
+++ plan_pkg/velocity/include/acc_node.h
@@ -0,0 +1,81 @@
+#include <sstream>
+#include <string>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <boost/filesystem.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/package.h>
+
+#include <eigen3/Eigen/Geometry>
+
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <autoware_msgs/DetectedObject.h>
+#include <autoware_msgs/DetectedObjectArray.h>
+#include <hmcl_msgs/Lane.h>
+#include <hmcl_msgs/Waypoint.h>
+#include <hmcl_msgs/VehicleWheelSpeed.h>
+
+class ACC 
+{
+
+    private:
+    ros::NodeHandle nh_;
+    ros::Publisher target_vel_pub, vel_debug;
+    ros::Subscriber pose_sub, acc_sub, target_sub;
+
+
+    float current_acc, current_vel, current_x, current_y;
+    float object_x, object_y, object_vel;
+
+
+    int delay_step;
+    double delay_in_sec, lag_tau, dt;
+    double Q_dis, Q_vel, r_weight;
+    double acc_cmd, d_safe, d_time;
+
+    std::vector<double> acc_buffer;
+
+    Eigen::VectorXd Xk;
+
+    Eigen::MatrixXd mcQ, mcR;  
+    Eigen::MatrixXd Abaroc, Bbaroc;
+    Eigen::MatrixXd Abarc, Bbarc;
+    Eigen::MatrixXd Ad, Bd;
+
+    Eigen::MatrixXd mcAd, mcBd; 
+    Eigen::MatrixXd mcP;
+
+
+    geometry_msgs::PoseStamped debug_msg;
+
+    public:
+    ACC();
+
+    void computeMatrices();
+    double computeGain();
+    
+    bool solveRiccatiIterationD(const Eigen::MatrixXd &Ad,
+            const Eigen::MatrixXd &Bd, const Eigen::MatrixXd &Q,
+            const Eigen::MatrixXd &R, Eigen::MatrixXd &P,
+            const double &tolerance = 1.E-5,
+            const uint iter_max = 100000);
+
+    void poseCallback(const nav_msgs::Odometry& state_msg);
+    void accCallback(const geometry_msgs::Point& msg);
+    void objectCallback(const autoware_msgs::DetectedObjectArray& msg);
+
+    void CalcVel();
+
+};
---
+++ plan_pkg/velocity/include/velocity_generator.h
@@ -0,0 +1,52 @@
+#include <sstream>
+#include <string>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <boost/filesystem.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/package.h>
+
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <hmcl_msgs/Lane.h>
+#include <hmcl_msgs/Waypoint.h>
+#include <hmcl_msgs/VehicleWheelSpeed.h>
+
+class VelocityGenerator 
+{
+
+    private:
+    ros::NodeHandle nh_;
+    ros::Publisher vel_pub, vel_vis_pub, vel_debug;
+    ros::Subscriber local_traj_sub, vel_sub, acc_sub, acc_target_vel_sub;
+
+
+    bool new_vel, local_traj_init, visualize, new_target_vel;
+    float speed_limit, current_acc, current_vel, target_acc_vel;
+
+    hmcl_msgs::Lane traj;
+    geometry_msgs::PoseStamped debug_msg;
+
+    public:
+    VelocityGenerator();
+
+
+    void trajCallback(const hmcl_msgs::Lane& msg);
+    void velCallback(const nav_msgs::Odometry& state_msg);
+    void accCallback(const geometry_msgs::Point& msg);
+    void targetCallback(const std_msgs::Float64& msg);
+
+    void CalcVel();
+    void viz_vel_prof(std::vector<double> profile);
+};
