3169d25599ad290119cacbba0d55acb4630387b2
amilearning <hojin.projects@gmail.com>
2022-10-19 23:34:16 +0900

    reboot py update

--- boot_pkg/boot/reboot/src/reboot.py
+++ boot_pkg/boot/reboot/src/reboot.py
@@ -5,7 +5,8 @@ import os
 import sys
 import rospy
 
-from . import config
+# from . import config
+import subprocess
 from std_msgs.msg import String
 
 class reboot():
@@ -13,6 +14,11 @@ class reboot():
         rospy.init_node('reboot', anonymous=True)
         # rospy.Subscriber("/global_waypoints_rviz", MarkerArray, self.tarjCallback)
         self.pub = rospy.Publisher('/this', String, queue_size=10)
+        pkg = "preview_ctrl"
+        launchfile = "preview_ctrl.launch"
+        command = "roslaunch {0} {1}".format(pkg, launchfile)
+        my_env = os.environ.copy()
+        p = subprocess.Popen(command, shell=True, env=my_env)
 
     def main(self):
         r = rospy.Rate(10) # 10hz
